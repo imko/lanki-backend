74685a4b187cbc389f1e3db9343980c696f9d7ef
Peter Ko <46854966+imko@users.noreply.github.com>
2024-01-25 19:28:51 -0800

    Add API gateway and implement retries, fallback and circuit breaker with resilience4j

---
+++ edge-service/src/test/java/io/lanki/edgeservice/EdgeServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package io.lanki.edgeservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EdgeServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
